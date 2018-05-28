58fe1c39ac089d2b6bb4b4c293ebd9d14ff457d0
asdfghwunai <1729589198@qq.com>
2018-05-28 09:35:45 +0800

    Create 二进制中1的个数.cpp

---
+++ bit operation/二进制中1的个数.cpp
@@ -0,0 +1,22 @@
+/*
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+*/
+
+/*----方法----
+注意操作前先把int转为unsigned int,因为对int负数移位和你想的不太一样，说什么最后会变成0xFFFFFFFF
+
+*/
+
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int result=0;
+         unsigned int number=n;
+         while(number!=0)
+         {
+             result+=number&1;
+             number=number>>1;
+         }
+         return result;
+     }
+};
