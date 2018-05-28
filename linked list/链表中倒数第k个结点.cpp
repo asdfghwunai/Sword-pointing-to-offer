f842b926faa8f68f113bebc6052d590e707bdc8a
asdfghwunai <1729589198@qq.com>
2018-05-28 16:18:23 +0800

    Create 链表中倒数第k个结点.cpp

---
+++ linked list/链表中倒数第k个结点.cpp
@@ -0,0 +1,37 @@
+/*
+输入一个链表，输出该链表中倒数第k个结点。
+*/
+
+/*----方法-----
+双指针，放一个先走到第k个结点，后面再开始走
+
+*/
+
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+        ListNode *fast=pListHead;
+        ListNode *slow=pListHead;
+        
+        for(int i=1;i<=k-1&&fast;i++)  //循环k-1次到第k个结点
+            fast=fast->next;
+        if(fast==NULL)    //就是fast直接指向NULL说明链表长度不够
+            return NULL;
+        
+        //while(fast)    //fast达到末尾时,slow正好差n+1-k步
+        while(fast->next)    //fast指向最后一个节点结束
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+};
