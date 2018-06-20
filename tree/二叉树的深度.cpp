d3b87bf3b864611c0787cf2469017f282bb87faf
asdfghwunai <1729589198@qq.com>
2018-06-20 15:16:48 +0800

    Create 二叉树的深度.cpp

---
+++ tree/二叉树的深度.cpp
@@ -0,0 +1,31 @@
+/*
+输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+*/
+
+/*
+不出意外地栈溢出,其实是写错了==写成=
+*/
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(pRoot==NULL)
+            return 0;
+        int left=TreeDepth(pRoot->left);
+        int right=TreeDepth(pRoot->right);
+        
+        return (left>right)?(left+1):(right+1);
+    }
+};
+
+
